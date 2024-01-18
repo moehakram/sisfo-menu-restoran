b05736a7bd2e248cdc9300343576f1cb194f4f71
gistbit <muh.akram1422@gmail.com>
2024-01-18 18:52:33 +0800

    update

--- config/constants.php
+++ config/constants.php
@@ -5,7 +5,7 @@ define('ROOT', str_replace('\\', '/', rtrim(__DIR__, '/')) . '/../');
 define('APP', ROOT . 'app/');
 define('CONTROLLERS', ROOT . 'app/Controllers/');
 define('MODELS', ROOT . 'app/Models/');
-define('VIEWS', ROOT . 'app/Views/');
+define('VIEWS', ROOT . 'app/views/');
 define('UPLOAD', ROOT . 'public/upload/');
 define('CONFIG', ROOT . 'config/');
 
